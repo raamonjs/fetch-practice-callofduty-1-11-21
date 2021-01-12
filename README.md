dd8e699a6bd87f78b0dad7d5b1f88d88b2691340
Ramon <raamonu@icloud.com>
2021-01-12 10:54:30 -0700

    Added an option to load player matches in UI. Need to fix minor UI bugs like removing old tables when client wants to search for a new player

--- README.md
+++ README.md
@@ -1,4 +1,11 @@
 # fetch-practice-callofduty-1-11-21
 
-This project is a learning opportunity for me to get a better feel for js using the Fetch API. 
+This project is a learning opportunity to get a better handle of js, Fetch API, and DOM Manipulation. It isn't meant to be structured. Although, may be rebuilt after learning React.
+
 Inspiration comes from sbmmwarzone.com
+
+Requires you to sign up with rapidapi.com (Requires to sign up with credit card to receive API Keys). Doesn't charge you unless you go above 500 requests/day.
+
+Full url of api https://rapidapi.com/elreco/api/call-of-duty-modern-warfare.
+
+Alternative from NPM can be found at https://www.npmjs.com/package/call-of-duty-api.
--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// // Old method retreiving API Keys before finally figuring out how to import js file.
 // // importing credentials
 // let credentials;
 // let xhr = new XMLHttpRequest();
@@ -10,16 +11,19 @@
 // };
 // xhr.send();
 // // needed to add time out in order to get credentials in time before http request
-// setTimeout(getStats, 2000);
+// setTimeout(getPlayerStats, 2000);
 
-// Forgot to add .js to import apikeys and can get credentials synchronously now instead of method above
+// Had trouble importing js file but needed to add '.js' extention to allow import
 import { apikeys as credentials } from './apikeys.js';
 
-// Using fetch api to make requests to call of duty api
+// Declaring variables outside of getPlayerStats() to be used in getPlayerMatches()
+let username, platform, URL;
+
+// Using fetch to make request and pass in object to updateUIWithPlayerStats()
 function getPlayerStats(e) {
-  let username = document.getElementById('username').value;
-  let platform = document.getElementById('platforms').value;
-  const URL = 'https://call-of-duty-modern-warfare.p.rapidapi.com';
+  username = document.getElementById('username').value;
+  platform = document.getElementById('platforms').value;
+  URL = 'https://call-of-duty-modern-warfare.p.rapidapi.com';
   // fetching player stats
   fetch(`${URL}/warzone/${username}/${platform}`, {
     method: 'GET',
@@ -34,39 +38,41 @@ function getPlayerStats(e) {
     })
     .then((obj) => {
       // calling updateui
-      updateUIWithPlayer(obj.br_all, username);
+      updateUIWithPlayerStats(obj.br_all, username);
     })
     .catch((err) => {
       console.error(err);
     });
 
-  // // fetching match stats
-  // // too many requests at once (even though it's just 2)
-  // fetch(
-  // `${URL}/warzone-matches/${username}/${platform}`,
-  //   {
-  //     method: 'GET',
-  //     headers: {
-  //       'x-rapidapi-key': credentials.key,
-  //       'x-rapidapi-host': credentials.host
-  //     }
-  //   }
-  // )
-  //   .then((response) => {
-  //     return response.json();
-  //   })
-  //   .then((obj) => {
-  //     updateUIWithMatches(obj.matches);
-  //   })
-  //   .catch((err) => {
-  //     console.error(err);
-  //   });
+  e.preventDefault();
+}
+
+// Using fetch to make request and pass in object to updateUIWithPlayerMatches()
+function getPlayerMatches(e) {
+  // fetching match stats
+  // too many requests at once (even though it's just 2) <-- before moving outside of getPlayerStats()
+  fetch(`${URL}/warzone-matches/${username}/${platform}`, {
+    method: 'GET',
+    headers: {
+      'x-rapidapi-key': credentials.key,
+      'x-rapidapi-host': credentials.host
+    }
+  })
+    .then((response) => {
+      return response.json();
+    })
+    .then((obj) => {
+      updateUIWithPlayerMatches(obj.matches);
+    })
+    .catch((err) => {
+      console.error(err);
+    });
 
   e.preventDefault();
 }
 
 // update ui with table of stats of player. will eventually add button to remove stats and hide table again.
-function updateUIWithPlayer(obj, username) {
+function updateUIWithPlayerStats(obj, username) {
   // console.log(obj);
 
   let content = `
@@ -77,17 +83,35 @@ function updateUIWithPlayer(obj, username) {
     <td>${obj.kills}</td>
     <td>${obj.downs}</td>
   </tr>`;
-  document.getElementById('table').removeAttribute('hidden');
-  document.getElementById('stats').innerHTML = content;
+  document.getElementById('table-stats').removeAttribute('hidden');
+  document.getElementById('stats-player').innerHTML = content;
 }
 
 // // update ui with table of stats of matches. will eventually add button to remove stats and hide table again.
-// function updateUIWithMatches(matches) {
-//   console.log(matches);
-// }
+function updateUIWithPlayerMatches(matches) {
+  let content = '';
+  console.log(matches);
+  matches.forEach((match) => {
+    content += `
+    <tr>
+      <th scope="row">${match.playerStats.teamPlacement}</th>
+      <td>${match.playerStats.kills}</td>
+      <td>${match.playerStats.damageDone}</td>
+      <td>${new Date(match.utcStartSeconds * 1000).toLocaleString()}</td>
+    </tr>`;
+  });
+  document.getElementById('table-matches').removeAttribute('hidden');
+  document.getElementById('stats-matches').innerHTML = content;
+  document.getElementById('load-matches').style.visibility = 'hidden';
+}
 
-// adding a event listener when submiting form
+// adding a event listener when submiting playerId and gaming platform
 document.getElementById('submit').addEventListener('click', getPlayerStats);
+
+// button appears after loading playerStats and can be clicked to show last 20 matches
+document
+  .getElementById('load-matches')
+  .addEventListener('click', getPlayerMatches);
 
 //////////////////////////////////////////////////////////////////////////////////
 
@@ -96,11 +120,14 @@ document.getElementById('submit').addEventListener('click', getPlayerStats);
 /*
   stats wanted
   array
-  element.playerStats.teamPlacement
-  element.playerStats.kills
-  element.playerStats.damageDone
-  // get time and date of match
-  time = element.utcStartSeconds
-  let date = new Date(1609823801 * 1000)
-  date.toDateString()
+  element.playerStats.teamPlacement // placement
+  element.playerStats.kills // kills
+  element.playerStats.damageDone // damage
+  // get time and date of match // time (code to get date below)
+  // time = element.utcStartSeconds
+  // let date = new Date(1609823801 * 1000) // or new Date(time * 1000)
+  // toLocaleString returns the date and time
+  // date.toLocaleString()
   */
+
+/////////////// UPDATE, FEATURE HAS BEEN ADDED ///////////////////////////////////
