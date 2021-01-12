6d4b4c2cc06e0951818579680ae7fd1c31118248
Ramon <raamonu@icloud.com>
2021-01-11 20:42:10 -0700

    was able to import keys and not have to use xmlhttprequest to parse a json file.

--- script.js
+++ script.js
@@ -1,14 +1,25 @@
-// importing credentials
-let credentials;
-let xhr = new XMLHttpRequest();
-xhr.open('GET', 'apikeys.json', true);
-xhr.onload = function () {
-  if (this.status === 200) {
-    credentials = JSON.parse(this.responseText);
-    // console.log(credentials);
-  }
-};
-xhr.send();
+// // importing credentials
+// let credentials;
+// let xhr = new XMLHttpRequest();
+// xhr.open('GET', 'apikeys.json', true);
+// xhr.onload = function () {
+//   if (this.status === 200) {
+//     credentials = JSON.parse(this.responseText);
+//     // console.log(credentials);
+//   }
+// };
+// xhr.send();
+
+// // needed to add time out in order to get credentials in time before http request
+// setTimeout(getStats, 2000);
+
+// import { apikeys } from './apikeys.js';
+
+// console.log(apikeys);
+
+// Forgot to add .js to import apikeys and can get credentials synchronously now instead of method above
+
+import { apikeys as credentials } from './apikeys.js';
 
 // Using fetch api to make requests to call of duty api
 function getStats() {
@@ -34,5 +45,4 @@ function getStats() {
     });
 }
 
-// needed to add time out in order to get credentials in time before http request
-setTimeout(getStats, 2000);
+getStats();
