3cceb9481cfee3d0a73aa2a0f7262b325d678b00
Ramon <raamonu@icloud.com>
2021-01-11 22:46:02 -0700

    added functionality for retrieving matches and future features. api only allows no more than one request at once. Might add separate button to show matches.

--- script.js
+++ script.js
@@ -9,22 +9,17 @@
 //   }
 // };
 // xhr.send();
-
 // // needed to add time out in order to get credentials in time before http request
 // setTimeout(getStats, 2000);
 
-// import { apikeys } from './apikeys.js';
-
-// console.log(apikeys);
-
 // Forgot to add .js to import apikeys and can get credentials synchronously now instead of method above
-
 import { apikeys as credentials } from './apikeys.js';
 
 // Using fetch api to make requests to call of duty api
-function getStats(e) {
+function getPlayerStats(e) {
   let username = document.getElementById('username').value;
   let platform = document.getElementById('platforms').value;
+  // fetching player stats
   fetch(
     `https://call-of-duty-modern-warfare.p.rapidapi.com/warzone/${username}/${platform}`,
     {
@@ -41,17 +36,40 @@ function getStats(e) {
     })
     .then((obj) => {
       // calling updateui
-      updateUI(obj.br_all, username);
+      updateUIWithPlayer(obj.br_all, username);
     })
     .catch((err) => {
       console.error(err);
     });
+
+  // // fetching match stats
+  // // too many requests at once (even though it's just 2)
+  // fetch(
+  // `https://call-of-duty-modern-warfare.p.rapidapi.com/warzone-matches/${username}/${platform}`,
+  //   {
+  //     method: 'GET',
+  //     headers: {
+  //       'x-rapidapi-key': credentials.key,
+  //       'x-rapidapi-host': credentials.host
+  //     }
+  //   }
+  // )
+  //   .then((response) => {
+  //     return response.json();
+  //   })
+  //   .then((obj) => {
+  //     updateUIWithMatches(obj.matches);
+  //   })
+  //   .catch((err) => {
+  //     console.error(err);
+  //   });
+
   e.preventDefault();
 }
 
-// update ui with table of stats. will eventually add button to remove stats and hide table again.
-function updateUI(obj, username) {
-  console.log(obj);
+// update ui with table of stats of player. will eventually add button to remove stats and hide table again.
+function updateUIWithPlayer(obj, username) {
+  // console.log(obj);
 
   let content = `
   <tr>
@@ -65,9 +83,26 @@ function updateUI(obj, username) {
   document.getElementById('stats').innerHTML = content;
 }
 
+// // update ui with table of stats of matches. will eventually add button to remove stats and hide table again.
+// function updateUIWithMatches(matches) {
+//   console.log(matches);
+// }
+
 // adding a event listener when submiting form
+document.getElementById('submit').addEventListener('click', getPlayerStats);
 
-document.getElementById('submit').addEventListener('click', getStats);
+//////////////////////////////////////////////////////////////////////////////////
 
-// How i'd get value of platform
-// console.log(document.getElementById('platforms').value);
+// feature to include a list of latest matches
+
+/*
+  stats wanted
+  array
+  element.playerStats.teamPlacement
+  element.playerStats.kills
+  element.playerStats.damageDone
+  // get time and date of match
+  time = element.utcStartSeconds
+  let date = new Date(1609823801 * 1000)
+  date.toDateString()
+  */
